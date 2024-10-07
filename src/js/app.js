671dedbae2a5bba45eb0114544a9856967d44cb9
Ivan <ivan.karabankov@gmail.com>
2024-10-07 18:28:13 +0300

    3. Made popup logic

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,35 @@ import * as functions from './modules/functions.js';
 // Checking if browsers supports .webp
 functions.isWebp();
 
+const dialog = document.querySelector('.popup');
+const closeModalBtn = dialog.querySelector('.popup__close');
+const openModalBtn = document.querySelector('.call');
+
+const closeOnOverlayClick = ({currentTarget, target}) => {
+  const dialog = currentTarget;
+  const isOverlayClick = target === dialog;
+
+  if (isOverlayClick) {
+    closeModal();
+  }
+}
+
+const openModalAndBlockScroll = () => {
+  dialog.showModal();
+  document.body.classList.add('scroll-block');
+}
+
+const returnScroll = () => {
+  document.body.classList.remove('scroll-block');
+}
+
+const closeModal = () => {
+  dialog.close();
+  returnScroll();
+}
+
+openModalBtn.addEventListener('click', openModalAndBlockScroll);
+closeModalBtn.addEventListener('click', closeModal);
+dialog.addEventListener('click', closeOnOverlayClick);
+dialog.addEventListener('cancel', returnScroll);
+
